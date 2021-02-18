add8055b440c99498aec38717287a2fdd433969e
AlbertoCaballero <albertocaballero80@outlook.com>
2021-02-18 17:57:48 -0600

    Fizz Buzz

---
+++ src/fizzbuzz.rs
@@ -0,0 +1,8 @@
+pub fn fizzbuzz(n: i32) -> i32 {
+    match (n % 3 == 0, n % 5 == 0) {
+        (true, false) => -1,
+        (false, true) => -2,
+        (true, true) => -1,
+        _ => n
+    }
+}
