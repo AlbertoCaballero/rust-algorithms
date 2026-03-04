7e7344711a17a67c56c6d25dfffbf7fa33fa4a65
AlbertoCaballero <albertoCaballero80@outlook.com>
2026-03-04 01:38:20 -0600

    Reference

--- src/reference.rs
+++ src/reference.rs
@@ -6,14 +6,25 @@ fn main() {
 
     s1 = do_something_moving(s1);
     println!("{}", s1);
+
+    do_something_referenced(&mut s1);
+    println!("{}", s1);
 }
 
+// param is mutable variable from s1, now s is owner untile returned
 fn do_something_moving(mut s: String) -> String {
     println!("Moved {}", s);
     s = String::from("moved");
     s
 }
 
+// s is only borrowing s1
 fn do_something_borrowed(s: &String) {
     println!("Borrowed {}", s)
+}
+
+// s is borrowing a mutable reference of s1
+fn do_something_referenced(s: &mut String) {
+    *s = String::from("referenced");
+    println!("Referenced {}", s)
 }
