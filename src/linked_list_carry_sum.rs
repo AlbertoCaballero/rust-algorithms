0f34b4fea1d8f2f0546c0eb1ebb796c6503bd8f8
AlbertoCaballero <albertoCaballero80@outlook.com>
2026-07-01 13:54:29 -0600

    Fix impl from leetcode

--- src/linked_list_carry_sum.rs
+++ src/linked_list_carry_sum.rs
@@ -14,28 +14,26 @@ impl ListNode {
     }
 }
 
-impl Solution {
-    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut l1 = l1;
-        let mut l2 = l2;
-        let mut carry = 0;
-        let mut holder = Box::new(ListNode::new(0));
-        let mut tail = &mut holder;
+pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut l1 = l1;
+    let mut l2 = l2;
+    let mut carry = 0;
+    let mut holder = Box::new(ListNode::new(0));
+    let mut tail = &mut holder;
 
-        while l1.is_some() || l2.is_some() || carry != 0 {
-            let v1 = l1.as_ref().map_or(0, |n| n.val);
-            let v2 = l2.as_ref().map_or(0, |n| n.val);
+    while l1.is_some() || l2.is_some() || carry != 0 {
+        let v1 = l1.as_ref().map_or(0, |n| n.val);
+        let v2 = l2.as_ref().map_or(0, |n| n.val);
 
-            let sum = v1 + v2 + carry;
-            carry = sum / 10;
+        let sum = v1 + v2 + carry;
+        carry = sum / 10;
 
-            tail.next = Some(Box::new(ListNode::new(sum % 10)));
-            tail = tail.next.as_mut().unwrap();
+        tail.next = Some(Box::new(ListNode::new(sum % 10)));
+        tail = tail.next.as_mut().unwrap();
 
-            l1 = l1.and_then(|n| n.next);
-            l2 = l2.and_then(|n| n.next);
-        }
-
-        holder.next
+        l1 = l1.and_then(|n| n.next);
+        l2 = l2.and_then(|n| n.next);
     }
+
+    holder.next
 }
